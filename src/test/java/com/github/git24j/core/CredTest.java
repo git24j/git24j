1484a4cbdc59f3b252756a339390ae6f10bd4c1a
Shijing Lu <shijing.lu@gmail.com>
2020-04-25 16:08:55 -0400

    Revspec tests

---
+++ src/test/java/com/github/git24j/core/CredTest.java
@@ -0,0 +1,45 @@
+package com.github.git24j.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CredTest extends TestBase {
+
+    @Test
+    public void userpassPlaintextNew() {
+        Cred cred = Cred.userpassPlaintextNew("user", "passwd");
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+
+    @Test
+    public void sshKeyNew() {
+        Cred cred = Cred.sshKeyNew("user", "/tmp/abc.pub", "/tmp/abc.priv", "passphrase");
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+
+    @Test
+    public void sshKeyFromAgent() {
+        Cred cred = Cred.sshKeyFromAgent("user");
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+
+    @Test
+    public void defaultNew() {
+        Cred cred = Cred.defaultNew();
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+
+    @Test
+    public void usernameNew() {
+        Cred cred = Cred.usernameNew("tester");
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+
+    @Test
+    public void sshKeyMemoryNew() {
+        Cred cred = Cred.sshKeyMemoryNew("username", "publickey", "privatekey", "passphrase");
+        Assert.assertTrue(cred.getRawPointer() > 0);
+    }
+}
---
+++ src/test/java/com/github/git24j/core/RevwalkTest.java
@@ -0,0 +1,59 @@
+package com.github.git24j.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RevwalkTest extends TestBase {
+
+    @Test
+    public void hide() {}
+
+    @Test
+    public void hideGlob() {}
+
+    @Test
+    public void hideHead() {}
+
+    @Test
+    public void testHideHead() {}
+
+    @Test
+    public void hideRef() {}
+
+    @Test
+    public void testHideRef() {}
+
+    @Test
+    public void create() {}
+
+    @Test
+    public void next() {}
+
+    @Test
+    public void push() {}
+
+    @Test
+    public void pushGlob() {}
+
+    @Test
+    public void pushHead() {}
+
+    @Test
+    public void pushRange() {}
+
+    @Test
+    public void pushRef() {}
+
+    @Test
+    public void repository() {}
+
+    @Test
+    public void reset() {}
+
+    @Test
+    public void simplifyFirstParent() {}
+
+    @Test
+    public void sorting() {}
+}
