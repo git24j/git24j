a56bc2cd61a701a25fe3c2a7e0273c0fe32ecd7f
Frank <noreplay.19288gjjdg4@gitnoreplay13948857882.com.baddomail>
2024-03-10 15:19:57 +0800

    MergeTest.java:  add merge branch test demo

--- src/test/java/com/github/git24j/core/MergeTest.java
+++ src/test/java/com/github/git24j/core/MergeTest.java
@@ -4,9 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
+import static com.github.git24j.core.Checkout.StrategyT.ALLOW_CONFLICTS;
+import static com.github.git24j.core.Checkout.StrategyT.FORCE;
 
 public class MergeTest extends TestBase {
     // git rev-parse HEAD^{tree}
@@ -16,6 +17,140 @@ public class MergeTest extends TestBase {
     private static final String SHA_DEV = "e5b28427ba064002e0e343e783ea3095018ce72c";
     private static final String SHA_C = "f80e0b10f83e512d1fae0142d000cceba3aca721";
     private static final String SHA_C_DEV_BASE = "67a36754360b373d391af2182f9ad8929fed54d8";
+
+    private static final String repoPath = "set to your repo path";
+
+    /**
+     * merge targetBranch into HEAD.
+     * note: this is a very base merge demo, even no fast-forward, you shouldn't use these code in production.
+     * a better example please check: https://github.com/libgit2/libgit2/blob/main/examples/merge.c
+     */
+    @Test
+    public void mergeTargetBranchIntoHead() {
+        String targetBranchShortHandName = "main";
+        Repository repo = Repository.open(repoPath);
+        Merge.Options mergeOpts = Merge.Options.create();
+        Repository.StateT state = repo.state();
+
+        mergeOpts.setFileFlags(1<<1);  //GIT_MERGE_FILE_STYLE_DIFF3
+        mergeOpts.setFlags(0);  // 0 should mean no flags?
+
+        Checkout.Options checkoutOpts = Checkout.Options.defaultOptions();
+        checkoutOpts.setStrategy(EnumSet.of(FORCE,ALLOW_CONFLICTS));
+
+        assert(state.getBit()==0); //expect 0 NONE
+        Reference targetBranchRef = Reference.dwim(repo, targetBranchShortHandName);
+        AnnotatedCommit mergeTarget = AnnotatedCommit.fromRef(repo, targetBranchRef);
+        System.out.println("mergeTarget.ref():"+mergeTarget.ref());
+        System.out.println("mergeTarget.id():"+mergeTarget.id());
+//        System.out.println("mergeInto(current branch):"+mergeInto.id());
+        ArrayList<AnnotatedCommit> acList = new ArrayList<>();
+        acList.add(mergeTarget);
+        Merge.AnalysisPair analysis = Merge.analysis(repo, acList);
+        //expect NORMAL
+        System.out.println("analysis.getAnalysis().name()::"+analysis.getAnalysis().name());
+        if(analysis.getAnalysis().getBit()!= Merge.AnalysisT.NORMAL.getBit()) {
+            System.out.println("repo state is not NORMAL! merge canceled!");
+            return;
+        }
+
+        //do merege
+        Merge.merge(repo,acList,mergeOpts,checkoutOpts);
+
+        if(repo.index().hasConflicts()) {
+            System.out.println("hasConflicts: "+repo.index().hasConflicts());
+            System.out.println("Please resolve conflicts then make a commit conclude the merge");
+        }else {
+            doCommitThenCleanRepoState(repo, mergeTarget);
+        }
+
+        //alert: close should in finally block when use lib in production, this is test, so whatever
+        repo.close();
+
+    }
+
+    private void doCommitThenCleanRepoState(Repository repo, AnnotatedCommit mergeTarget) {
+        Signature sign = Signature.create("me", "me@invalidemailaddress19384774.com");
+
+        // merged node should have two parents, HEAD and targetBranch.
+        ArrayList<Commit> commitList = new ArrayList<>();
+        Commit commit1 = Commit.lookup(repo,Reference.dwim(repo, "HEAD").id());
+        Commit commit2 = Commit.lookup(repo, mergeTarget.id());
+        commitList.add(commit1);
+        commitList.add(commit2);
+        System.out.println("commit ID1:"+commit1.id());
+        System.out.println("commit ID2:"+commit2.id());
+        String branchFullRefName = Reference.dwim(repo,"HEAD").name();
+        System.out.println("branchFullRefName:"+branchFullRefName);
+
+        //write repo's index as a tree
+        Tree tree = Tree.lookup(repo, repo.index().writeTree());
+
+        //commit
+        Oid resultCommitOid = Commit.create(repo, branchFullRefName, sign, sign, null, "merge branch",
+                tree, commitList);  //问题在于这里
+        System.out.println("New Commit Oid is:"+resultCommitOid);
+
+        //clear repo state, else repo will stay "Merging" state
+        repo.stateCleanup();
+        repo.close();
+    }
+
+    @Test
+    public void printRepoConflictItems() {
+        Repository repo = Repository.open(repoPath);
+        boolean hasConflict = repo.index().hasConflicts();
+        System.out.println("repo has conflict:"+hasConflict);
+        if(!hasConflict) {
+            return;
+        }
+        Index.ConflictIterator conflictIterator = repo.index().conflictIteratorNew();
+        Index.Conflict item = conflictIterator.next();
+        while (item!=null) {
+            String conflictFilePath = item.our.getPath();
+            System.out.println("conflict file(both modified):"+conflictFilePath);
+            item = conflictIterator.next();
+        }
+        repo.close();
+    }
+
+    @Test
+    public void indexAddAll(){
+        Repository repo = Repository.open(repoPath);
+        repo.index().write();  //write to disk
+//        repo.index().writeTree();//get an oid for create commit
+        repo.close();
+    }
+
+    @Test
+    public void doCommitThenCleanRepoState() {
+        //merged node should have two parents, HEAD and merge target branch,
+        // `mergeTargetName` should set to merge target branch name
+        String mergeTargetName = "main";
+
+        Repository repo = Repository.open(repoPath);
+        AnnotatedCommit mergeTarget = AnnotatedCommit.fromRef(repo, Reference.dwim(repo, mergeTargetName));
+        Signature sign = Signature.create("me", "me@invalidemailaddress19384774.com");
+        ArrayList<Commit> parents = new ArrayList<>();
+        parents.add(Commit.lookup(repo, repo.head().id()));  //commit success but parents are wrong.
+        parents.add(Commit.lookup(repo, mergeTarget.id()));
+
+        Tree tree = Tree.lookup(repo, repo.index().writeTree());
+
+        //create commit
+        Commit.create(repo, repo.head().name(),sign,sign,null,"merge branch",
+                tree,parents);
+
+        repo.stateCleanup();
+        repo.close();
+    }
+
+    @Test
+    public void stateCleanup() {
+        Repository repo = Repository.open(repoPath);
+        repo.stateCleanup();
+        repo.close();
+    }
 
     @Test
     public void base() {
